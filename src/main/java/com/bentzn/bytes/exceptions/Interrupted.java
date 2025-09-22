ef4fca015d4f221ab85bc725ea673afe08ea09d0
bentzn <th@bentzn.com>
2025-09-22 13:49:54 +0100

    Further refined

--- src/main/java/com/bentzn/bytes/exceptions/Interrupted.java
+++ src/main/java/com/bentzn/bytes/exceptions/Interrupted.java
@@ -7,24 +7,46 @@ public class Interrupted {
             public void run() {
                 try {
                     System.out.println("Starting thread...");
+                    System.out.println("    interrupted: " + isInterrupted());
+                    System.out.println("    alive:       " + isAlive());
                     
                     // Now we interrupt the thread
                     interrupt();
                     
                     // ...but the thread actually continues!
-                    System.out.println("After interrupt: " + isInterrupted());
-                    
+                    System.out.println("After interrupt");
+                    System.out.println("    interrupted: " + isInterrupted());
+                    System.out.println("    alive:       " + isAlive());
+
                     // ...until it reaches somewhere, that _can_ be interrupted
                     sleep(50);
                 }
                 catch (InterruptedException e) {
                     e.printStackTrace();
+                    System.out.println("After interrupt");
+                    System.out.println("    interrupted: " + isInterrupted());
+                    System.out.println("    alive:       " + isAlive());
                 }
+                
+                try {
+                    // The interrupted status is reset after the InterruptedException was thrown
+                    sleep(50);
+                }
+                catch (InterruptedException e) {
+                    System.out.println("Un-expected InterruptedException");
+                }
+                
+                System.out.println("Thread finished");
             }
         };
         
         thr.start();
         thr.join();
+        
+        System.out.println("After join");
+        System.out.println("    interrupted: " + thr.isInterrupted());
+        System.out.println("    alive:       " + thr.isAlive());
+
     }
     
     
