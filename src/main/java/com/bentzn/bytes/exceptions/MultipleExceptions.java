1f76103ef86b01db42d3bab1de507b407af9b237
bentzn <th@bentzn.com>
2025-09-22 12:39:47 +0100

    Added MultipleExceptions

---
+++ src/main/java/com/bentzn/bytes/exceptions/MultipleExceptions.java
@@ -0,0 +1,41 @@
+package com.bentzn.bytes.exceptions;
+
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+
+public class MultipleExceptions {
+
+    private String getFromServer(String request) throws DataFormatException {
+        // gets something from a server - can throw a DataFormatException
+        return "result from server";
+    }
+
+
+
+    private void persistSomething(String something) throws IOException {
+        // will persist to disk
+        // Let's imagine this operation is not essential (caching)
+    }
+
+
+
+    String storeSomething(String result) throws IOException, DataFormatException {
+        String result2 = getFromServer(result);
+        persistSomething(result2);
+        return result2;
+    }
+}
+
+class Caller {
+    public static void main(String[] args) {
+        try {
+            new MultipleExceptions().storeSomething("request 123");
+        }
+        catch (IOException e) {
+            // Alert the user that it was not possible to store Something
+        }
+        catch (DataFormatException e) {
+            // No need to alert the user
+        }
+    }
+}
